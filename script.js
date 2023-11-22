e1be9516462741b7327fad54b7c1912e8d2d864c
ayukrisn <ayuukrisn@gmail.com>
2023-11-23 00:47:41 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const validateEmail = (event) => {
+    event.preventDefault(); // Prevent default form submission behavior
+
+    const input = document.getElementById("email").value;
+    const filter = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    if (input.match(filter)) {
+        document.querySelector(".success-state").style.display = "inline-block";
+        document.querySelector(".desc-container").style.display = "none";
+        document.querySelector(".email-form").style.display = "none";
+        document.querySelector(".success-state").innerHTML = "THANKS FOR SUBSCRIBE!!!";
+    } else {
+        document.querySelector(".error-state").style.display = "inline-block";
+        document.querySelector(".error-state").innerHTML = "Valid Email Required!";
+        document.querySelector('input[type="text"]').style.backgroundColor = "#FFB4B4";
+    }
+}
